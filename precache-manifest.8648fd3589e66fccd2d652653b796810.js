6629c784b6c0f1bfdd1f48d43b4cebd532467cb2
venkat-logixal <57442414+venkat-logixal@users.noreply.github.com>
2020-01-23 16:37:52 +0530

    Add files via upload

---
+++ precache-manifest.8648fd3589e66fccd2d652653b796810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f083068525085e941bb08786c0a5186",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eca911d0903dd1b27361",
+    "url": "./static/js/2.36014d90.chunk.js"
+  },
+  {
+    "revision": "7860d4ef06be155ac7179c75c0e06cc5",
+    "url": "./static/js/2.36014d90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ddd6666094d7c4baec4",
+    "url": "./static/js/main.8860d527.chunk.js"
+  },
+  {
+    "revision": "33674f35b625edc56232",
+    "url": "./static/js/runtime-main.ccb8a94e.js"
+  },
+  {
+    "revision": "1bf67806c3278e6bdf3a3228909e4b68",
+    "url": "./static/media/logo.1bf67806.png"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "./static/media/logo.a0185b04.svg"
+  }
+]);
